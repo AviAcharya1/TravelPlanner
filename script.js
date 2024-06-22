2458be5f36c021024813f4d52b9bdddef11daf92
aviacharya01 <aviacharyasr@gmail.com>
2024-06-22 16:19:33 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+const navToggleEvent = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+};
+
+navElemArr.forEach((elem) => {
+  elem.addEventListener("click", navToggleEvent);
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", navToggleEvent);
+});
+
+/**
+ * go to top & header active
+ */
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 200) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
